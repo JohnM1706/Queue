f0ad4c0b310a304d96f6222231b0ab5d5f392763
john fredy mosquera acosta <jhonfred680@gmail.com>
2020-11-18 01:00:49 -0500

    manejo de Queue

---
+++ quiz_queue/Quiz_Queue.java
@@ -0,0 +1,11 @@
+
+package quiz_queue;
+
+public class Quiz_Queue {
+
+    public static void main(String[] args) {
+        segundario pantalla = new segundario();
+        pantalla.setVisible(true);
+    }
+    
+}
